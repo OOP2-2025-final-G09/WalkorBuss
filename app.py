ea78a308004f901c48b712348e98725ca78d6f7f
Haruhisa222 <k24008kk@aitech.ac.jp>
2026-01-01 02:58:51 +0900

    first commit
    天気・温度をAPIで取得(index.htmlに簡易的に表示)

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 # Flaskサーバー
 from flask import Flask, render_template
+from weather import get_weather
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    weather, temp = get_weather()
+    return render_template(
+        "index.html",
+        weather=weather,  # これでweatherがindex.htmlで使えるようになる
+        temp=temp  # これでtempがindex.htmlで使えるようになる
+    )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
+
--- weather.py
+++ weather.py
@@ -1,1 +1,26 @@
 # 天気API
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+def get_weather():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": "Okazaki,jp",
+        "units": "metric",
+        "appid": API_KEY,
+        "lang": "ja"
+    }
+
+    res = requests.get(url, params=params)
+    data = res.json()
+    #print(data)
+
+    weather = data["weather"][0]["description"]
+    temp = data["main"]["temp"]
+
+    return weather, temp
